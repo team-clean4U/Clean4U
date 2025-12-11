d739ed347dc2b5e98afe95bc406bc29de3c88965
kwonding <wldo8541@naver.com>
2025-12-11 16:28:28 +0900

    feat: Customer entity 생성(BE)

--- clean4u/src/main/java/org/example/clean4u/customer/Customer.java
+++ clean4u/src/main/java/org/example/clean4u/customer/Customer.java
@@ -1,4 +1,39 @@
 package org.example.clean4u.customer;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Data
+@Table(name = "customer_tb")
+@Entity
 public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birth", nullable = false)
+    private LocalDate birth;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "grade")
+    private Grade grade;
+
+    @Column(name = "memo", length = 1000)
+    private String memo;
+
+    public Customer(String name, LocalDate birth, String phone, Grade grade) {
+        this.name = name;
+        this.birth = birth;
+        this.phone = phone;
+        this.grade = grade;
+    }
 }
