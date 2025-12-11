db71f0d5392d6469547c1d1d707e6ec0e173ef12
ses9133 <ses9133@gmail.com>
2025-12-11 16:24:26 +0900

    feat: Order Entity 생성(BE)

--- clean4u/src/main/java/org/example/clean4u/order/Order.java
+++ clean4u/src/main/java/org/example/clean4u/order/Order.java
@@ -3,6 +3,10 @@ package org.example.clean4u.order;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.clean4u.customer.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "order_tb")
@@ -13,4 +17,10 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order_tb", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<OrderItem> orderItem = new ArrayList<>();
 }
