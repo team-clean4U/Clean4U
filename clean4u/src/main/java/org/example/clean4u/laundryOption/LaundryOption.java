3fdddbb739461a97a778102956da5c71223a52c5
kimgsun <gsune@kakao.com>
2025-12-11 16:31:08 +0900

    feat: LaundryOption 엔티티 추가(BE)

---
+++ clean4u/src/main/java/org/example/clean4u/laundryOption/LaundryOption.java
@@ -0,0 +1,29 @@
+package org.example.clean4u.laundryOption;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "laundry_option_tb")
+public class LaundryOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer extraprice;
+
+    @Builder
+    public LaundryOption(Long id, String name, Integer extraprice) {
+        this.id = id;
+        this.name = name;
+        this.extraprice = extraprice;
+    }
+}
