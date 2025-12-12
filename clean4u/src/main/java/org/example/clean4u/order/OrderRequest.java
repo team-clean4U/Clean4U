9212c308fe6cbcfa3cec79ce2b3fe9e1677c427c
ses9133 <ses9133@gmail.com>
2025-12-12 16:33:04 +0900

    feat: Order 엔티티, DTO 수정(BE)

--- clean4u/src/main/java/org/example/clean4u/order/OrderRequest.java
+++ clean4u/src/main/java/org/example/clean4u/order/OrderRequest.java
@@ -1,24 +1,50 @@
 package org.example.clean4u.order;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.example.clean4u.customer.Customer;
 
 import java.time.LocalDate;
 
 public class OrderRequest {
     @Data
-    public static class UpdateDto {
+    public static class SaveDto {
+        @NotNull(message = "고객 id를 입력하세요")
+        Long customerId;
+
+        @NotNull(message = "주문 상태를 입력하세요")
         OrderStatus status;
+
+        @NotNull(message = "총 금액을 입력하세요")
         Long totalPrice;
+
+        @NotNull(message = "주문 날짜를 입력하세요")
         LocalDate orderDate;
+
         String memo;
 
-        public void validate() {
-            if(status == null) {
-                throw new IllegalArgumentException("주문 상태를 입력하세요");
-            }
-            if(totalPrice == null) {
-                throw new IllegalArgumentException("총 금액을 입력하세요");
-            }
+        public Order toEntity(Customer customer) {
+            return Order.builder()
+                    .customer(customer)
+                    .status(this.status)
+                    .totalPrice(this.totalPrice)
+                    .orderDate(this.orderDate)
+                    .memo(this.memo)
+                    .build();
         }
+    }
+
+    @Data
+    public static class UpdateDto {
+        @NotNull(message = "주문 상태를 입력하세요")
+        OrderStatus status;
+
+        @NotNull(message = "총 금액을 입력하세요")
+        Long totalPrice;
+
+        @NotNull(message = "주문 날짜를 입력하세요")
+        LocalDate orderDate;
+
+        String memo;
     }
 }
