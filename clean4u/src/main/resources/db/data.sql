4f5791c8f4ba474bda09fcb9dc9f91aeca747b26
kimgsun <gsune@kakao.com>
2025-12-13 03:22:09 +0900

    chore: 데이터베이스 목데이터 추가(BE)

--- clean4u/src/main/resources/db/data.sql
+++ clean4u/src/main/resources/db/data.sql
@@ -9,3 +9,12 @@ insert into laundry_option_tb(name, extra_price) values
                                                      ('표백', 2000),
                                                      ('향 추가', 1000),
                                                      ('프리미엄 세제', 6000);
+
+insert into supply_item_tb(name, unit, stock_quantity) values
+                                                           ('세탁세제', 'ml', 50000),
+                                                           ('섬유유연제', 'ml', 30000),
+                                                           ('표백제', 'ml', 20000),
+                                                           ('비닐포장지', 'ea', 1000),
+                                                           ('종이포장봉투', 'ea', 500),
+                                                           ('옷걸이', 'ea', 300),
+                                                           ('세탁망', 'ea', 150);
