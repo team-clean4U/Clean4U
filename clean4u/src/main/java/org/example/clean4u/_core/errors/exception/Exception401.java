d1d7855f9910a614a45df6a1423989724643b01c
ses9133 <ses9133@gmail.com>
2025-12-12 15:58:07 +0900

    feat: Custom Error 정의(BE)

---
+++ clean4u/src/main/java/org/example/clean4u/_core/errors/exception/Exception401.java
@@ -0,0 +1,7 @@
+package org.example.clean4u._core.errors.exception;
+
+public class Exception401 extends RuntimeException {
+    public Exception401(String message) {
+        super(message);
+    }
+}
---
+++ clean4u/src/main/java/org/example/clean4u/_core/errors/exception/Exception403.java
@@ -0,0 +1,7 @@
+package org.example.clean4u._core.errors.exception;
+
+public class Exception403 extends RuntimeException{
+    public Exception403(String message) {
+        super(message);
+    }
+}
