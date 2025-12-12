10b8b1ffdb7c132dd7c00d00650c844cbdb3ce60
kwonding <wldo8541@naver.com>
2025-12-12 15:26:36 +0900

    feat: Customer dto 생성(BE)

--- clean4u/src/main/java/org/example/clean4u/customer/CustomerRequest.java
+++ clean4u/src/main/java/org/example/clean4u/customer/CustomerRequest.java
@@ -1,4 +1,38 @@
 package org.example.clean4u.customer;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
 public class CustomerRequest {
+
+    @Data
+    public static class saveDto {
+        private String name;
+        private LocalDate birth;
+        private String phone;
+        private String memo;
+
+        public Customer toEntity() {
+            return new Customer(name, birth, phone, memo);
+        }
+    }
+
+    @Data
+    public static class updateDto {
+        private String name;
+        private LocalDate birth;
+        private String phone;
+        private String memo;
+
+        public void validate() {
+            if (name == null || name.trim().isEmpty()) {
+                throw new IllegalArgumentException("이름은 필수입니다.");
+            }
+
+            if (phone == null || phone.trim().isEmpty()) {
+                throw new IllegalArgumentException("휴대폰 번호는 필수입니다.");
+            }
+        }
+    }
 }
