6bcc8b83b1578b3b49f6a732b4ce809f4ad0ed78
kwonding <wldo8541@naver.com>
2025-12-12 15:26:02 +0900

    feat: Customer Controller 생성(BE)

--- clean4u/src/main/java/org/example/clean4u/customer/CustomerController.java
+++ clean4u/src/main/java/org/example/clean4u/customer/CustomerController.java
@@ -1,4 +1,12 @@
 package org.example.clean4u.customer;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
 public class CustomerController {
+    private final CustomerPersistRepository repository;
+
+
 }
