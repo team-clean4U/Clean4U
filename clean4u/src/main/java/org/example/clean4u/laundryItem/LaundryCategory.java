d896816609cf302148a2a13ac44ab2812e4c58f8
kimgsun <gsune@kakao.com>
2025-12-14 02:12:03 +0900

    feat: LaundryItem 카테고리 아이콘 매핑 추가(BE)

--- clean4u/src/main/java/org/example/clean4u/laundryItem/LaundryCategory.java
+++ clean4u/src/main/java/org/example/clean4u/laundryItem/LaundryCategory.java
@@ -1,12 +1,21 @@
 package org.example.clean4u.laundryItem;
 
 public enum LaundryCategory {
-    TOP,
-    BOTTOM,
-    OUTER,
-    UNDEREWEAR,
-    BEDDING,
-    ACCESSORY,
-    SPECIAL,
-    ETC
+    TOP("shirt"),
+    BOTTOM("pants"),
+    OUTER("vest"),
+    BEDDING("bed"),
+    ACCESSORY("hat-cowboy"),
+    SPECIAL("star"),
+    ETC("box");
+
+    private final String icon;
+
+    LaundryCategory(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
 }
