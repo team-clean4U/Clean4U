f5ca4447bc95e87ee2c4fee7b6102f63a10f429e
kimgsun <gsune@kakao.com>
2025-12-16 20:00:55 +0900

    feat: LaundryOption 리포지토리 JPA 상속 형태로 변경(BE)

--- clean4u/src/main/java/org/example/clean4u/laundryOption/LaundryOptionRepository.java
+++ clean4u/src/main/java/org/example/clean4u/laundryOption/LaundryOptionRepository.java
@@ -1,56 +1,14 @@
 package org.example.clean4u.laundryOption;
 
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.example.clean4u._core.exception.Exception404;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-public class LaundryOptionRepository {
+public interface LaundryOptionRepository extends JpaRepository<LaundryOption, Long> {
+    List<LaundryOption> findByIsActive(Boolean isActive);
 
-    private final EntityManager em;
+    List<LaundryOption> findByNameContaining(String name);
 
-    @Transactional
-    public LaundryOption save(LaundryOption laundryOption) {
-        em.persist(laundryOption);
-        return laundryOption;
-    }
-
-    public List<LaundryOption> findAll() {
-        return em.createQuery(
-                "SELECT lo FROM LaundryOption lo ORDER BY id DESC",
-                LaundryOption.class
-        ).getResultList();
-    }
-
-    public LaundryOption findById(Long id) {
-        LaundryOption laundryOption = em.find(LaundryOption.class, id);
-        if (laundryOption == null) {
-            throw new Exception404("해당 세탁물 옵션을 찾을 수 없습니다.");
-        }
-        return laundryOption;
-    }
-
-    @Transactional
-    public LaundryOption updateById(Long id, LaundryOptionRequest.UpdateDTO updateDTO) {
-        LaundryOption laundryOption = em.find(LaundryOption.class, id);
-        if (laundryOption == null) {
-            throw new Exception404("해당 세탁물 옵션을 찾을 수 없습니다.");
-        }
-        laundryOption.update(updateDTO);
-        return laundryOption;
-    }
-
-    @Transactional
-    public void deleteById(Long id) {
-        LaundryOption laundryOption = em.find(LaundryOption.class, id);
-        if (laundryOption == null) {
-            throw new Exception404("해당 세탁물 옵션을 찾을 수 없습니다.");
-        }
-        em.remove(laundryOption);
-    }
+    Optional<LaundryOption> findByName(String name);
 }
