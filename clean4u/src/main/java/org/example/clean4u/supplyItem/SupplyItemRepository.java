f19ba3384ef31241c2fcc9afb7aa4e986133c9b3
kimgsun <gsune@kakao.com>
2025-12-17 04:42:34 +0900

    feat: SupplyItem respository 메서드 보완(BE)

--- clean4u/src/main/java/org/example/clean4u/supplyItem/SupplyItemRepository.java
+++ clean4u/src/main/java/org/example/clean4u/supplyItem/SupplyItemRepository.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SupplyItemRepository extends JpaRepository<SupplyItem, Long> {
+    @Query("SELECT si FROM SupplyItem si ORDER BY si.createdAt DESC")
+    List<SupplyItem> findAllOrderByCreatedAtDesc();
+
     @Query("SELECT si FROM SupplyItem si WHERE si.stockQuantity <= si.safetyStock")
     List<SupplyItem> findLowStockItems();
 
