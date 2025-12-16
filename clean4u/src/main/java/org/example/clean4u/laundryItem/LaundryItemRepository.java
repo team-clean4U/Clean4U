fc16e6157b140aaa44ace3a128b58f4dcf71e980
kimgsun <gsune@kakao.com>
2025-12-16 20:00:27 +0900

    feat: LaundryItem 리포지토리 JPA 상속 형태로 변경(BE)

--- clean4u/src/main/java/org/example/clean4u/laundryItem/LaundryItemRepository.java
+++ clean4u/src/main/java/org/example/clean4u/laundryItem/LaundryItemRepository.java
@@ -1,56 +1,14 @@
 package org.example.clean4u.laundryItem;
 
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.example.clean4u._core.exception.Exception404;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-public class LaundryItemRepository {
+public interface LaundryItemRepository extends JpaRepository<LaundryItem, Long> {
+    List<LaundryItem> findByCategory(LaundryCategory category);
 
-    private final EntityManager em;
+    List<LaundryItem> findByNameContaining(String name);
 
-    @Transactional
-    public LaundryItem save(LaundryItem laundryItem) {
-        em.persist(laundryItem);
-        return laundryItem;
-    }
-
-    public List<LaundryItem> findAll() {
-        return em.createQuery(
-                        "SELECT li FROM LaundryItem li ORDER BY li.id DESC"
-                        , LaundryItem.class)
-                .getResultList();
-    }
-
-    public LaundryItem findById(Long id) {
-        LaundryItem laundryItem = em.find(LaundryItem.class, id);
-        if (laundryItem == null) {
-            throw new Exception404("해당 세탁물을 찾을 수 없습니다.");
-        }
-        return laundryItem;
-    }
-
-    @Transactional
-    public LaundryItem updateById(Long id, LaundryItemRequest.UpdateDTO reqDTO) {
-        LaundryItem laundryItem = em.find(LaundryItem.class, id);
-        if (laundryItem == null) {
-            throw new Exception404("해당 세탁물을 찾을 수 없습니다.");
-        }
-        laundryItem.update(reqDTO);
-        return laundryItem;
-    }
-
-    @Transactional
-    public void deleteById(Long id) {
-        LaundryItem laundryItem = em.find(LaundryItem.class, id);
-        if (laundryItem == null) {
-            throw new Exception404("해당 세탁물을 찾을 수 없습니다.");
-        }
-        em.remove(laundryItem);
-    }
+    Optional<LaundryItem> findByName(String name);
 }
