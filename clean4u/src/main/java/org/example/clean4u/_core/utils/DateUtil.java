cd44f4db196df99188fa23fe3b922d9323f7166f
kwonding <wldo8541@naver.com>
2025-12-16 14:52:20 +0900

    feat: DateUtil 추가(BE)

--- clean4u/src/main/java/org/example/clean4u/_core/utils/DateUtil.java
+++ clean4u/src/main/java/org/example/clean4u/_core/utils/DateUtil.java
@@ -1,16 +1,25 @@
 package org.example.clean4u._core.utils;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter BIRTHFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static String timestampFormat(Timestamp time) {
         if (time == null) {
             return null;
         }
         return time.toLocalDateTime().format(FORMATTER);
+    }
+
+    public static String birthFormat(LocalDate time) {
+        if (time == null) {
+            return null;
+        }
+        return time.format(BIRTHFORMATTER);
     }
 }
