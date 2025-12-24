b7f7b5d7cab13d5ed85c00e22d131044411d4a2a
ses9133 <ses9133@gmail.com>
2025-12-24 17:40:42 +0900

    feat: Dashboard 구조 정의(BE)

--- clean4u/src/main/java/org/example/clean4u/dashboard/DashboardController.java
+++ clean4u/src/main/java/org/example/clean4u/dashboard/DashboardController.java
@@ -1,4 +1,16 @@
 package org.example.clean4u.dashboard;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping("/dashboard/")
+    public String dashboard() {
+        return "dashboard/list";
+    }
 }
--- clean4u/src/main/java/org/example/clean4u/dashboard/DashboardService.java
+++ clean4u/src/main/java/org/example/clean4u/dashboard/DashboardService.java
@@ -1,4 +1,11 @@
 package org.example.clean4u.dashboard;
 
+import lombok.RequiredArgsConstructor;
+import org.example.clean4u.order.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DashboardService {
+    private final OrderRepository orderRepository;
 }
