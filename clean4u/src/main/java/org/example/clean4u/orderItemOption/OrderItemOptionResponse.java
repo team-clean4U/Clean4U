17a8011ade82ecc0a1ea059ed85344571ac84664
ses9133 <ses9133@gmail.com>
2025-12-24 17:40:01 +0900

    chore: 코드 컨벤션 통일(BE)

--- clean4u/src/main/java/org/example/clean4u/orderItemOption/OrderItemOptionResponse.java
+++ clean4u/src/main/java/org/example/clean4u/orderItemOption/OrderItemOptionResponse.java
@@ -1,17 +1,19 @@
 package org.example.clean4u.orderItemOption;
 
 import lombok.Data;
+import org.example.clean4u._core.utils.PriceUtil;
 
 public class OrderItemOptionResponse {
     @Data
     public static class DetailDto {
         private String optionName;
-        private Integer extraPrice;
+        private String extraPrice;
 
         public DetailDto(OrderItemOption orderItemOption) {
             if (orderItemOption.getLaundryOption() != null) {
                 this.optionName = orderItemOption.getLaundryOption().getName();
-                this.extraPrice = orderItemOption.getLaundryOption().getExtraPrice();
+                int extraPriceInt = orderItemOption.getLaundryOption().getExtraPrice();
+                this.extraPrice = PriceUtil.format(extraPriceInt);
             }
         }
     }
