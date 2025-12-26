aef2e804a5bbddaa79e34e1100566efd6ade6654
kimgsun <gsune@kakao.com>
2025-12-26 10:47:56 +0900

    feat: SupplyItemHistory 엔티티 및 타입 추가(BE)

---
+++ clean4u/src/main/java/org/example/clean4u/supplyItemHistory/Type.java
@@ -0,0 +1,17 @@
+package org.example.clean4u.supplyItemHistory;
+
+public enum Type {
+    IN("입고"),
+    OUT("출고"),
+    ADJUSTMENT("조정");
+
+    private final String description;
+
+    Type(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
