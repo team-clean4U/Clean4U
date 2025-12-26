5ca75dae3b08dc8c5f7ef8dcbe64ed3c9f8b68b5
ses9133 <ses9133@gmail.com>
2025-12-26 15:07:48 +0900

    feat: UpdatedAt 오류 수정(BE)

--- clean4u/src/main/java/org/example/clean4u/time/BaseTimeEntity.java
+++ clean4u/src/main/java/org/example/clean4u/time/BaseTimeEntity.java
@@ -1,14 +1,17 @@
 package org.example.clean4u.time;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 public class BaseTimeEntity {
     @CreationTimestamp
