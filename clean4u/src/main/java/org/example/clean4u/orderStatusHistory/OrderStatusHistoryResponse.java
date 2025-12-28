463ffaf62a35064fbb9e33264ab09ec32e4dbc1f
ses9133 <ses9133@gmail.com>
2025-12-28 20:46:15 +0900

    feat: 주문 상태 변경 확인 기능 추가(BE)

--- clean4u/src/main/java/org/example/clean4u/orderStatusHistory/OrderStatusHistoryResponse.java
+++ clean4u/src/main/java/org/example/clean4u/orderStatusHistory/OrderStatusHistoryResponse.java
@@ -1,4 +1,37 @@
 package org.example.clean4u.orderStatusHistory;
 
-public class OrderStatusReponse {
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.clean4u._core.utils.DateUtil;
+import org.example.clean4u.order.OrderStatus;
+
+import java.sql.Timestamp;
+
+public class OrderStatusHistoryResponse {
+    @Data
+    @NoArgsConstructor
+    public static class DetailDTO {
+        private Long orderId;
+        private OrderStatus status;
+        private String editor;
+        private String createdAt;
+
+        @Builder
+        public DetailDTO(Long orderId, OrderStatus status, String editor, Timestamp createdAt) {
+            this.orderId = orderId;
+            this.status = status;
+            this.editor = editor;
+            this.createdAt = DateUtil.timestampFormat(createdAt);
+        }
+
+        public static DetailDTO from(OrderStatusHistory history) {
+            return DetailDTO.builder()
+                    .orderId(history.getOrder().getId())
+                    .status(history.getStatus())
+                    .editor(history.getEditor().getName())
+                    .createdAt(history.getCreatedAt())
+                    .build();
+        }
+    }
 }
