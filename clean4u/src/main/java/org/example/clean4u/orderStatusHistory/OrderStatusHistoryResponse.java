5c6c5fac89961378f0f2306f4beffc7de9192a1b
ses9133 <ses9133@gmail.com>
2025-12-29 17:29:35 +0900

    feat: DTO 필드 추가(BE)

--- clean4u/src/main/java/org/example/clean4u/orderStatusHistory/OrderStatusHistoryResponse.java
+++ clean4u/src/main/java/org/example/clean4u/orderStatusHistory/OrderStatusHistoryResponse.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.clean4u._core.utils.DateUtil;
+import org.example.clean4u.order.Order;
 import org.example.clean4u.order.OrderStatus;
 
 import java.sql.Timestamp;
@@ -15,13 +16,19 @@ public class OrderStatusHistoryResponse {
         private Long orderId;
         private OrderStatus status;
         private String editor;
+        private String customerName;
+        private String phone;
         private String createdAt;
 
         @Builder
-        public DetailDTO(Long orderId, OrderStatus status, String editor, Timestamp createdAt) {
+        public DetailDTO(Long orderId, OrderStatus status, String editor, Order order, Timestamp createdAt) {
             this.orderId = orderId;
             this.status = status;
             this.editor = editor;
+            if(order != null) {
+                this.customerName = order.getCustomer().getName();
+                this.phone = order.getCustomer().getPhone();
+            }
             this.createdAt = DateUtil.timestampFormat(createdAt);
         }
 
@@ -30,6 +37,7 @@ public class OrderStatusHistoryResponse {
                     .orderId(history.getOrder().getId())
                     .status(history.getStatus())
                     .editor(history.getEditor().getName())
+                    .order(history.getOrder())
                     .createdAt(history.getCreatedAt())
                     .build();
         }
