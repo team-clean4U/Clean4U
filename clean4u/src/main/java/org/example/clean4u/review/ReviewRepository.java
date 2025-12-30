4e5a374372bf8ca1261533b1551462b647dc30bb
kimgsun <gsune@kakao.com>
2025-12-30 22:04:30 +0900

    feat: Review 도메인 추가(BE)

---
+++ clean4u/src/main/java/org/example/clean4u/review/ReviewRepository.java
@@ -0,0 +1,10 @@
+package org.example.clean4u.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Optional<Review> findByOrderId(Long orderId);
+    boolean existsByOrderId(Long orderId);
+}
