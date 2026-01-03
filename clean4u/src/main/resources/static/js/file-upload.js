bee0857b6eca490646cc302a5942b685f496a676
ses9133 <ses9133@gmail.com>
2026-01-03 18:11:08 +0900

    feat: 주문 이미지 삭제 UI 구현(FE)

--- clean4u/src/main/resources/static/js/file-upload.js
+++ clean4u/src/main/resources/static/js/file-upload.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("laundryImage");
     const fileName = document.getElementById("fileName");
-    const imageLabel = document.getElementById("image-label");
+    const imageLabel = document.getElementById("imageLabel");
+
+    if(!input || !fileName || !imageLabel) return;
 
     input.addEventListener("change", () => {
-        fileName.textContent = input.files ? input.files[0].name : ' 선택된 파일 없음';
-        imageLabel.innerHTML = '<i class="fa-solid fa-edit"></i> 파일 수정';
+        if(input.files && input.files[0]) {
+            fileName.textContent = input.files[0].name;
+            imageLabel.innerHTML = '<i class="fa-solid fa-edit"></i> 파일 수정';
+        } else {
+            fileName.textContent = " 선택된 파일 없음";
+        }
     });
 });
