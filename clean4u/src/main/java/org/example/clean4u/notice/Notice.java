5ecacbb3f2bb812867499f4cfe394240f6c534cc
kwonding <wldo8541@naver.com>
2026-01-03 21:52:23 +0900

    feat:MultipartFile 로직 추가(BE)

--- clean4u/src/main/java/org/example/clean4u/notice/Notice.java
+++ clean4u/src/main/java/org/example/clean4u/notice/Notice.java
@@ -9,6 +9,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -28,6 +30,14 @@ public class Notice {
     @JoinColumn(name = "employee_id", nullable = false)
     private Employee employee;
 
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "notice_image_tb",
+            joinColumns = @JoinColumn(name = "notice_id")
+    )
+    @Column(name = "image_name")
+    private List<String> noticeImages = new ArrayList<>();
+
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private Timestamp createdAt;
@@ -37,14 +47,47 @@ public class Notice {
     private Timestamp updatedAt;
 
     @Builder
-    public Notice(String title, String content, Employee employee) {
+    public Notice(String title, String content, Employee employee, List<String> noticeImages) {
         this.title = title;
         this.content = content;
         this.employee = employee;
+        this.noticeImages = noticeImages;
     }
 
     public void update(NoticeRequest.UpdateDTO updateDTO) {
         this.title = updateDTO.getTitle();
         this.content = updateDTO.getContent();
+    }
+
+    public void clearImages() {
+        this.noticeImages.clear();
+    }
+
+    public void addImages(List<String> images) {
+        if (images == null || images.isEmpty()) {
+            this.noticeImages.addAll(images);
+        }
+    }
+
+    public List<String> getNoticeImages() {
+        if (this.noticeImages == null || this.noticeImages.isEmpty()) {
+            return List.of();
+        }
+
+        List<String> noticeImagePaths = new ArrayList<>();
+
+        for (String image: this.noticeImages) {
+            if (image == null) {
+                continue;
+            }
+
+            if (image.startsWith("http")) {
+                noticeImagePaths.add(image);
+            } else {
+                noticeImagePaths.add("/images/notice" + image);
+            }
+        }
+
+        return noticeImagePaths;
     }
 }
