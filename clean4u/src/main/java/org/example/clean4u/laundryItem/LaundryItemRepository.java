a5b8fc92a126682fb4821f8e9d11539262003a58
kimgsun <gsune@kakao.com>
2026-01-15 07:14:43 +0900

    feat: LaundryItem 비활성화 상태 관리 기능 추가(BE)

--- clean4u/src/main/java/org/example/clean4u/laundryItem/LaundryItemRepository.java
+++ clean4u/src/main/java/org/example/clean4u/laundryItem/LaundryItemRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,8 +19,15 @@ public interface LaundryItemRepository extends JpaRepository<LaundryItem, Long> 
 
     Page<LaundryItem> findByNameContaining(String name, Pageable pageable);
 
-    @Query("SELECT li FROM LaundryItem li WHERE li.name LIKE CONCAT('%', :name, '%') AND li.category = :category ORDER BY li.createdAt DESC")
-    Page<LaundryItem> findByNameContainingAndCategory(@Param("category") LaundryCategory category, @Param("name") String name, Pageable pageable);
+    Page<LaundryItem> findByNameContainingAndCategory(String name, LaundryCategory category, Pageable pageable);
+
+    Page<LaundryItem> findByIsActive(Boolean isActive, Pageable pageable);
+
+    Page<LaundryItem> findByNameContainingAndIsActive(String name, Boolean isActive, Pageable pageable);
+
+    Page<LaundryItem> findByCategoryAndIsActive(LaundryCategory category, Boolean isActive, Pageable pageable);
+
+    Page<LaundryItem> findByNameContainingAndCategoryAndIsActive(String name, LaundryCategory category, Boolean isActive, Pageable pageable);
 
     Optional<LaundryItem> findByName(String name);
 }
