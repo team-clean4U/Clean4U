fd03216b5c72d3a34b6eb95288f08755898f084c
kwonding <wldo8541@naver.com>
2026-01-18 12:00:33 +0900

    feat: detailDTO에 파일추가(BE)

--- clean4u/src/main/java/org/example/clean4u/notice/NoticeResponse.java
+++ clean4u/src/main/java/org/example/clean4u/notice/NoticeResponse.java
@@ -3,6 +3,7 @@ package org.example.clean4u.notice;
 import lombok.Data;
 import org.example.clean4u._core.utils.DateUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NoticeResponse {
@@ -33,7 +34,7 @@ public class NoticeResponse {
         private String writer;
         private String content;
         private String createdAt;
-        private List<String> uploadImages;
+        private List<NoticeFileDTO> attachments;
 
         public DetailDTO(Notice notice){
             this.id = notice.getId();
@@ -45,7 +46,22 @@ public class NoticeResponse {
             if (notice.getCreatedAt() != null) {
                 this.createdAt = DateUtil.timestampFormat(notice.getCreatedAt());
             }
-            this.uploadImages = notice.getNoticeImagePath();
+            this.attachments = notice.getNoticeFiles().stream()
+                    .map(NoticeFileDTO::new)
+                    .toList();
+        }
+    }
+
+    @Data
+    public static class NoticeFileDTO {
+        private Long fileId;
+        private String originalName;
+        private Long fileSize;
+
+        public NoticeFileDTO(NoticeFile noticeFile){
+            this.fileId = noticeFile.getId();
+            this.originalName = noticeFile.getOriginalName();
+            this.fileSize = noticeFile.getFileSize();
         }
     }
 }
