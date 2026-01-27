44d4cc5b7bbea67384e0ffdc60a60b6335065ff3
kwonding <wldo8541@naver.com>
2026-01-27 16:21:31 +0900

    fix:중복JS 하나로 병합(FE)

--- clean4u/src/main/resources/static/js/customer/update.js
+++ clean4u/src/main/resources/static/js/customer/update.js
@@ -3,9 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if (form) {
         form.addEventListener("submit", async (e) => {
             e.preventDefault();
-            const formData = new FormData(form);
-            const customerId = window.location.pathname.match(/\/(\d+)\/edit/)[1];
 
+            const customerId = window.location.pathname.match(/\/(\d+)\/edit/)[1];
+            const phoneInput = document.getElementById("phone");
+
+            if (!/^010-\d{4}-\d{4}$/.test(phoneInput.value)) {
+                alert("연락처 형식이 올바르지 않습니다.");
+                phoneInput.focus();
+                return;
+            }
+
+            const formData = new FormData(form);
             const data = {
                 name: formData.get("name"),
                 phone: formData.get("phone"),
@@ -22,11 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
                     body: JSON.stringify(data)
                 });
 
-                if (!response.ok) {
+                if (response.ok) {
+                    alert("수정이 완료되었습니다");
+                    window.location.href = `/customers/${customerId}`;
+
+                } else {
                     const errorBody = await response.json();
                     throw new Error(errorBody.message);
-                } else {
-                    window.location.href = "/customers";
                 }
             } catch (error) {
                 alert(error.message);
